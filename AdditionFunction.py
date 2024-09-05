73cfc9440142ce89e4c330c45ac9e5a723e5a8ca
Shivam Missar <shivam.missar@nuh.nhs.uk>
2024-09-05 10:45:33 +0100

    Created two basic functions
    Created addition and string print function to test first Action created

---
+++ AdditionFunction.py
@@ -0,0 +1,7 @@
+def add_numbers(a , b):
+    return a + b
+
+
+if __name__=="__main__":
+    result =  add_numbers(10,10)
+    print(result)
---
+++ StringFunction.py
@@ -0,0 +1,16 @@
+
+def stringFunction(message: str):
+    print(message)
+
+
+
+if __name__=="__main__":
+    stringFunction("Print String")
+
+
+
+
+
+
+
+
