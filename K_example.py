e70f97d5964cc65ba0c1020831c2b927fbe01910
Shivam Missar <shivam.missar@nuh.nhs.uk>
2024-09-06 13:28:39 +0100

    Create K_example.py

---
+++ K_example.py
@@ -0,0 +1,11 @@
+
+
+# This is a comment with a forbidden K number: K1234567
+K_number_in_comment = "This is a test K number: K9876543"
+
+def example_function():
+    # Hardcoded K number in a variable
+    K_variable = "K3156882"
+    print(K_variable)  # Hardcoded K number in a print statement
+
+example_function()
