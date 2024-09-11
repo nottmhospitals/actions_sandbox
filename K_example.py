a95fade7bb51da1087789ab94f89b2ff7fa5cef2
ZiadNHS <ziad.ahmed@nhs.net>
2024-09-11 10:19:57 +0100

    Update K_example.py

--- K_example.py
+++ K_example.py
@@ -2,10 +2,13 @@
 
 # This is a comment with a forbidden K number: K1234567
 K_number_in_comment = "This is a test K number: K9876543"
+K_num_in_lowercase = k9876543
 
 def example_function():
     # Hardcoded K number in a variable
     K_variable = "K3156882"
-    print(K_variable)  # Hardcoded K number in a print statement
+    lowerK = "k3186082"
+    print(K_variable)# Hardcoded K number in a print statement
+    print(lowerK)# lower case
 
 example_function()
