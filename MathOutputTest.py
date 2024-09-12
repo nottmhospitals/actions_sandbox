224d35ced75e0dcc5f39e4749cc0bbfc23818aee
Shivam Missar <shivam.missar@nuh.nhs.uk>
2024-09-12 10:55:52 +0100

    Update MathOutputTest.py

--- MathOutputTest.py
+++ MathOutputTest.py
@@ -1,12 +1,9 @@
 def add_numbers(a , b):
     return a + b
      
-
+##
 if __name__=="__main__":
     function_pass =  add_numbers(10,10)
-    function_failed =  add_numbers(10,10)
 
     assert function_pass == 20,"Test passed expected 20"
-    assert function_failed == 30,"Test failed expected 20"
     print(function_pass)
-    print(function_failed)
