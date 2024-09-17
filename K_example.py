3ef646be5ede6d0d7efaef367a385d37ffb8790a
Shivam Missar <shivam.missar@nuh.nhs.uk>
2024-09-17 11:27:18 +0100

    Update K_example.py

--- K_example.py
+++ K_example.py
@@ -5,7 +5,7 @@ K_number_in_comment = "This is a test K number: K9876543"
 
 def example_function():
     # Hardcoded K number in a variable
-    K_variable = "K3156882"
+    K_variable = "K3156883"
     print(K_variable)  # Hardcoded K number in a print statement
 #call the function to test
 #ABTU
